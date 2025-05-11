0dda3d315c23e08ab1b68686b8b95f0b16d9a236
Rizki Maulana <rrizki402@gmail.com>
2025-05-12 00:48:16 +0700

    fix: remove leftover functions folder

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -5,12 +5,21 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAR8w6cIWcg7tgT-OlOH_evqqzuN6FHTcQ",
-  authDomain: "layananjasa-8ed57.firebaseapp.com",
-  projectId: "layananjasa-8ed57",
-  storageBucket: "layananjasa-8ed57.appspot.com",
-  messagingSenderId: "306221228089",
-  appId: "1:306221228089:web:902ad16b4bedd833beede8",
+  // apiKey: "AIzaSyAR8w6cIWcg7tgT-OlOH_evqqzuN6FHTcQ",
+  // authDomain: "layananjasa-8ed57.firebaseapp.com",
+  // projectId: "layananjasa-8ed57",
+  // storageBucket: "layananjasa-8ed57.appspot.com",
+  // messagingSenderId: "306221228089",
+  // appId: "1:306221228089:web:902ad16b4bedd833beede8",
+  
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  
+  
 };
 
 const app = initializeApp(firebaseConfig);
